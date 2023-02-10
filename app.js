cb33cc746ce5559f645175778cf73a26eaefd1b0
Savio-5 <s3.silveira@gmail.com>
2023-02-10 14:28:05 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+require('dotenv').config();
+
+const session = require('express-session');
+const passport = require('passport');
+
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: true }
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/admin', require('./routes/admin'))
+app.use('/api/user', require('./routes/user'))
+
+
+app.listen(process.env.PORT, () => {
+        console.log(`Server is listening on port ${process.env.PORT}`);
+    }
+);
---
+++ routes/auth.js
@@ -0,0 +1,188 @@
+const {v4} = require('uuid');
+const express = require('express');
+const passport = require('passport');
+const LocalStrategy = require('passport-local');
+const bcrypt = require('bcrypt');
+const db = require('../db/connectdb');
+
+passport.use(
+  "local",
+  new LocalStrategy(
+    {
+        usernameField : 'emailid',
+        passwordField : 'password',
+        passReqToCallback: true
+    },
+    function (req, emailid, password, done) {
+        if(req.body.role === "admin"){
+            db.query(
+                "SELECT * FROM admin WHERE emailid = ?",
+                [emailid],
+                
+                function (err, row) {
+                  if (err) {
+                    return done(err);
+                  }
+                  if (!row.length) {
+                    return done(null, false, {
+                      message: "Incorrect username or password.",
+                    });
+                  }
+                  if (!bcrypt.compareSync(password, row[0].password.toString("utf8"))) {
+                    return done(null, false, {
+                      message: "Incorrect username or password.",
+                    });
+                  }
+                  return done(null, row[0]);
+                }
+              );
+        }
+        if(req.body.role === "user"){
+          console.log("user")
+            db.query(
+                "SELECT * FROM users WHERE emailid = ?",
+                [emailid],
+                
+                function (err, row) {
+                  if (err) {
+                    return done(err);
+                  }
+                  if (!row.length) {
+                    return done(null, false, {
+                      message: "Incorrect username or password.",
+                    });
+                  }
+                  if (!bcrypt.compareSync(password, row[0].password.toString("utf8"))) {
+                    return done(null, false, {
+                      message: "Incorrect username or password.",
+                    });
+                  }
+                  return done(null, row[0]);
+                }
+              );
+        }
+  })
+);
+
+passport.serializeUser(function(user, cb) {
+  process.nextTick(function() {
+    cb(null, { id: user.id, emailid: user.emailid, name: user.name, role : user.role });
+  });
+});
+
+passport.deserializeUser(function(user, cb) {
+  process.nextTick(function() {
+    return cb(null, user);
+  });
+});
+
+
+const router = express.Router();
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+    if(req.isAuthenticated() && req.user.role == "admin"){
+        res.redirect('/admin');
+    }
+    if(req.isAuthenticated() && req.user.role == "user"){
+        res.redirect('/user');
+    }
+});
+
+router.post('/logout', function(req, res, next) {
+  req.logout(function(err) {
+    req.session.destroy();
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+});
+
+
+router.post("/signup", passport.authenticate("local-signup"), (req, res) => {
+    if(req.isAuthenticated() && req.user.role == "admin"){
+        res.redirect('/admin');
+    }
+    if(req.isAuthenticated() && req.user.role == "user"){
+        res.redirect('/user');
+    }
+}
+);
+
+passport.use(
+  "local-signup",
+  new LocalStrategy(
+    {
+        usernameField : 'emailid',
+        passwordField : 'password',
+        passReqToCallback: true
+    },
+    function (req, emailid, password, done) {
+        if(req.body.role === "admin"){
+            db.query(
+                "SELECT * FROM admin WHERE emailid = ?",
+                [emailid],
+                function (err, rows) {
+                  if (err) return done(err);
+                  if (rows.length) {
+                    return done(null, false, {
+                      message: "That username is already taken.",
+                    });
+                  } else {
+                    const salt = 8;
+                    const newUserMysql = {
+                      adminid : v4(),
+                      admin_name : req.body.fname + " " + req.body.lname,
+                      emailid: emailid,
+                      password: bcrypt.hashSync(password, salt)
+                    };
+        
+                    db.query(
+                      "INSERT INTO admin (adminid,admin_name,emailid,password) values (?,?,?,?)",
+                      [newUserMysql.id,newUserMysql.admin_name, newUserMysql.emailid, newUserMysql.password],
+                      function (err, rows) {
+                        newUserMysql.id = rows.insertId;
+                        return done(null, newUserMysql);
+                      }
+                    );
+                  }
+                }
+              );
+        }
+
+        if(req.body.role === "user"){
+            db.query(
+                "SELECT * FROM users WHERE emailid = ?",
+                [emailid],
+                function (err, rows) {
+                  if (err) return done(err);
+                  if (rows.length) {
+                    return done(null, false, {
+                      message: "That username is already taken.",
+                    });
+                  } else {
+                    const salt = 8;
+                    const newUserMysql = {
+                        id : v4(),
+                        name : req.body.fname + " " + req.body.lname,
+                      emailid: emailid,
+                      role: req.body.role,
+                      password: bcrypt.hashSync(password, salt)
+                    };
+        
+                    db.query(
+                      "INSERT INTO users (id,name,emailid,password,role) values (?,?,?,?,?)",
+                      [newUserMysql.id,newUserMysql.name, newUserMysql.emailid, newUserMysql.password,newUserMysql.role],
+                      function (err, rows) {
+                        newUserMysql.id = rows.insertId;
+                        return done(null, newUserMysql);
+                      }
+                    );
+                  }
+                }
+              );
+        }
+      
+    }
+  )
+);
+
+module.exports = router;
